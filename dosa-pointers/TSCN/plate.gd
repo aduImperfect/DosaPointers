752ec4da27c7ac3a1a993eca750266ae1a37b5a8
Aditya Subramanian <adu101992@gmail.com>
2026-07-03 19:18:32 +0530

    Fixed debug error references tied with artifact texture loaded into the plate scene

--- dosa-pointers/TSCN/plate.gd
+++ dosa-pointers/TSCN/plate.gd
@@ -8,29 +8,31 @@ static var texturePlatesB : Array[Texture]
 static var platesACount : int = 0
 static var platesBCount : int = 0
 
-@export var plateVal : int = 0:
-	set(value):
-		plateVal = value
-		
-		# 2. Trigger your in-editor update function
-		_refresh_editor_view() 
-		
-		# 3. Force the Inspector UI panel to visually update
-		notify_property_list_changed() 
+@export var plateVal : int = 0
+#:
+	#set(value):
+		#plateVal = value
+		#
+		## 2. Trigger your in-editor update function
+		#_refresh_editor_view() 
+		#
+		## 3. Force the Inspector UI panel to visually update
+		#notify_property_list_changed() 
 
-@export var teamNo : int = 0:
-	set(value):
-		teamNo = value
-		
-		# 2. Trigger your in-editor update function
-		_refresh_editor_view() 
-		
-		# 3. Force the Inspector UI panel to visually update
-		notify_property_list_changed() 
+@export var teamNo : int = 0
+#:
+	#set(value):
+		#teamNo = value
+		#
+		## 2. Trigger your in-editor update function
+		#_refresh_editor_view() 
+		#
+		## 3. Force the Inspector UI panel to visually update
+		#notify_property_list_changed() 
 
 var sprite : Sprite2D
 
-static var initial_setup : bool = false
+#static var initial_setup : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -55,11 +57,11 @@ func _process(_delta: float) -> void:
 		#initial_setup = true
 
 	#MAKE SURE TO TURN THIS ON - IF PLAYING IN GAME AND TURN OFF THE ONE UNDER THE PROCESS FUNCTION
-	#_assign_plate_texture()
+	_assign_plate_texture()
 	pass
 
 func _assign_plate_texture() -> void:
-	if teamNo < 0 || teamNo > 2:
+	if teamNo < 0 || teamNo > 1:
 		print("Cannot assign team. Resetting to team A")
 		teamNo = 0
 
@@ -80,8 +82,8 @@ static func _load_textures_b() -> void:
 	for k in platesBCount:
 		texturePlatesB.append(load("res://Textures/PlateB_" + str(k) + ".png"))
 
-func _refresh_editor_view() -> void:
-	#MAKE SURE TO TURN THIS OFF - IF PLAYING IN GAME AND TURN ON THE ONE UNDER THE PROCESS FUNCTION
-	_assign_plate_texture()
-	#print("REFRESH EDITOR VIEW!")
-	pass
+#func _refresh_editor_view() -> void:
+	##MAKE SURE TO TURN THIS OFF - IF PLAYING IN GAME AND TURN ON THE ONE UNDER THE PROCESS FUNCTION
+	##_assign_plate_texture()
+	##print("REFRESH EDITOR VIEW!")
+	#pass
