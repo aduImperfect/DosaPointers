3b5931ba86f1f91057758e723d20ed1f05e9505c
Aditya Subramanian <adu101992@gmail.com>
2026-07-03 16:49:24 +0530

    Adding a background.

--- dosa-pointers/TSCN/level_gen.gd
+++ dosa-pointers/TSCN/level_gen.gd
@@ -1,17 +1,26 @@
 extends Node2D
 
+const BACKGROUND_SCENE = preload("res://TSCN/background.tscn")
 const LEVEL_SCENE = preload("res://TSCN/level.tscn")
 
 @export var xLevelCenter : float = 0.0
 @export var yLevelCenter : float = 0.0
 
+@export var xBackCenter : float = 0.0
+@export var yBackCenter : float = 0.0
+
 @export var levelNode : Node2D
+@export var backgroundNode : Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	xLevelCenter = 0.0
 	yLevelCenter = 0.0
 
+	xBackCenter = 0.0
+	yBackCenter = 0.0
+
+	_spawn_background()
 	_spawn_level()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,3 +33,10 @@ func _spawn_level() -> void:
 	level_instance.global_position.y = yLevelCenter
 	add_child(level_instance)
 	levelNode = level_instance
+
+func _spawn_background() -> void:
+	var background_instance = BACKGROUND_SCENE.instantiate()
+	background_instance.global_position.x = xBackCenter
+	background_instance.global_position.y = yBackCenter
+	add_child(background_instance)
+	backgroundNode = background_instance
